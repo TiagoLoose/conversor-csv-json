eff2b7fedd751d7a2a0193e2ffce86386c5106b3
TiagoLoose <tiagoloose@gmail.com>
2020-10-31 16:42:34 -0400

    Projeto versao 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,86 @@
+import { TextField, Typography, AppBar, Toolbar } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import { useState } from 'react';
+
+import './app.css'
+import conversor from './conversor';
+
 function App() {
+  const [csv, setCsv] = useState('')
+  const [json, setJson] = useState('')
+
+  function handleCsvChange(e){
+    setCsv(e.target.value)
+  }
+
+  function handleJsonChange(e){
+    setJson(e.target.value)
+  }
+
+  async function converterParaJson(){
+    try{
+      let json = await conversor.csv2Json(csv);
+      setJson(json)
+    }catch(error){
+      alert(error.message)
+    }
+  }
+
+  async function converterParaCsv(){
+    try{
+      let csv = await conversor.json2Csv(json);
+      setCsv(csv)
+    }catch (error){
+      alert(error.message)
+    }
+  }
+
   return (
-    <div className="App">
-      {'CSV<->JSON'}
+    <div id="container">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Conversor CSV JSON
+          </Typography >
+        </Toolbar>
+      </AppBar>
+      <div id="input-container">
+        <div id="csv-container">
+          <div>
+            <TextField
+              id="csv-input"
+              label="CSV"
+              multiline
+              variant="outlined"
+              rows="20"
+              fullWidth
+              value={csv}
+              onChange={handleCsvChange}
+            />
+          </div>
+          <div id="csv-buttons">
+            <Button variant="contained" color="primary" onClick={converterParaJson}>Converter para JSON</Button>
+          </div>
+        </div>
+
+        <div id="json-container">
+          <div>
+            <TextField
+              id="json-input"
+              label="JSON"
+              multiline
+              variant="outlined"
+              rows="20"
+              fullWidth
+              value={json}
+              onChange={handleJsonChange}
+            />
+          </div>
+          <div id="json-buttons">
+            <Button variant="contained" color="primary" onClick={converterParaCsv}>Converter para CSV</Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/conversor.js
@@ -0,0 +1,11 @@
+let converter = require('json-2-csv');
+
+async function csv2Json(csv){
+    return JSON.stringify(await converter.csv2jsonAsync(csv))
+}
+
+async function json2Csv(json){
+    return await converter.json2csvAsync(JSON.parse(json))
+}
+
+export default {json2Csv, csv2Json}
